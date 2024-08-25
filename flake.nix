9dd4b65deae93038a186f721a34f664097019b96
hkalbasi <hamidrezakalbasi@protonmail.com>
2024-08-25 21:45:08 +0330

    add dioxus-cli to flake

--- flake.nix
+++ flake.nix
@@ -1,3 +1,31 @@
+# in flake.nix
 {
-  outputs = inputs: { };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          nativeBuildInputs = with pkgs; [ dioxus-cli ];
+          buildInputs = with pkgs; [ openssl ];
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            # 👇 and now we can just inherit them
+            inherit buildInputs nativeBuildInputs;
+          };
+          devShells.android = mkShell
+            {
+              # 👇 and now we can just inherit them
+              inherit buildInputs;
+              nativeBuildInputs = nativeBuildInputs ++ [ pkgs.cowsay ];
+            };
+        }
+      );
 }
